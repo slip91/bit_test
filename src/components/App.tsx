46a048bcdd04fdeef3167aef17cbe0ae912157e9
ovz <slip912@gmail.com>
2019-10-17 10:31:13 +0400

    add component dir

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 import { YMaps, Map, Placemark } from "react-yandex-maps";
+import MapCar from "./mapCar/MapCar";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
@@ -61,6 +62,8 @@ class App extends React.Component< IProps, IState> {
     private render() {
         return (
             <div className="app">
+                <MapCar></MapCar>
+
                 <input value={this.state.input} onChange={this.handleChange.bind(this)} />
                 <YMaps query ={{
                     apikey: "75297380-b1bd-4ffc-a589-654d79516174",
---
+++ src/components/mapCar/MapCar.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+interface IProps {}
+
+interface IState {
+    coord: [];
+    ymaps: any;
+    input: string;
+}
+
+class MapCar extends React.Component< IProps, IState> {
+    public render() {
+        return (
+            <div className="app">
+                map
+            </div>
+        );
+    }
+}
+
+declare let module: object;
+
+export default hot(module)(MapCar);
